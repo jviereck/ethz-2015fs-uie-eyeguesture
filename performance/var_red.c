1e58e9296ea7feb9019604554fc4d3be55410747
Julian Viereck <julian.viereck@gmail.com>
2015-08-19 12:01:31 +0200

    Got the first c-implementation working

--- performance/var_red.c
+++ performance/var_red.c
@@ -2,7 +2,27 @@
 
 void myprint(void);
 
+float var_red(int N, float* arr);
+
+
 void myprint()
 {
     printf("hello world\n");
+}
+
+
+float var_red(int N, float* arr)
+{
+	float sum = 0.0;
+	float Nf = N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			sum += (arr[i] - arr[j]) * (arr[i] - arr[j]);
+		}
+	}
+
+	float res = sum / (Nf * Nf) ;
+	// printf("Get N=%d %f -> res=%f\n", N, arr[0], res);
+	return res;
 }
