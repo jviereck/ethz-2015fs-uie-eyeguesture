ef4d3de2f8b0ae77a7637d879d165e4d042c1636
Julian Viereck <julian.viereck@gmail.com>
2015-08-27 11:48:35 +0200

    Update extraction of faces using OpenCV ViolaJones

--- tools_lfpw/face_detection.py
+++ tools_lfpw/face_detection.py
@@ -20,6 +20,27 @@ def face_contains(face, px, py):
     (x, y, w, h) = face
     return x <= px and px <= (x + w) and y <= py and y <= (py + h)
 
+
+def find_face(faces, nose_x, nose_y):
+    candidate = None
+
+    for face in faces:
+        # import pdb; pdb.set_trace()
+        if not face_contains(face, nose_x, nose_y): # and face_contains(face, chin_x, chin_y)):
+            continue
+
+        # If there is already a candidate face AND the detected face is larger,
+        # then use the larger face.
+        # From experiments it turns out the larger face is normally better.
+        if not (candidate is None) and face[3] > candidate[3]:
+            candidate = None
+
+        if candidate is None:
+            candidate = face
+
+    return candidate
+
+
 def print_usage():
     usage = """
 Usage:
@@ -29,25 +50,17 @@ Usage:
 
 
 if __name__ == '__main__':
-    if (len(sys.argv) == 1):
-        print_usage()
-        sys.exit(1)
+    # if (len(sys.argv) == 1):
+    #     print_usage()
+    #     sys.exit(1)
 
-    input_dir = sys.argv[1]
-    idata = np.genfromtxt(join(input_dir, 'idata.csv'), delimiter=',', dtype='str')
+    # input_dir = sys.argv[1]
+    input_dir = 'img_sub'
 
-    # Take the x and the y coordinates. For this, compute the indices to keep.
-    t = idata.shape[1] / 3
-    x_index = np.linspace(0, t-1, t).astype('int') * 3 + 2
-    y_index = np.linspace(0, t-1, t).astype('int') * 3 + 3
-
-    # Concat the x and y indices and then sort them.
-    index = np.sort(np.r_[x_index, y_index])
-
-    # Do the final lookup, which yields the x and y coordinates of the landmarks
-    # nicely one after the other :)
-    data = np.c_[idata[:, index], np.zeros((1132, 4))].astype('float')
-
+    # Read the data with the landmarks from 'data.csv' and add extra columns
+    # to store the position of the face.
+    data = np.genfromtxt(join(input_dir, 'data.csv'), delimiter=',')
+    data = np.c_[data, np.zeros((data.shape[0], 4))]
 
     # Run the face detection for all the image files found in the folder.
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -64,53 +77,33 @@ if __name__ == '__main__':
         img = cv2.imread(join(input_dir, file_name))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray, 1.5, 3)
-
-        if len(faces) == 0:
-            # Retry with a smaller scaling factor.
-            faces = face_cascade.detectMultiScale(gray, 1.1, 3)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 3)
 
         if len(faces) == 0:
             print 'Could not find face for %s' % (file_name)
             continue
 
-        # for (x,y,w,h) in faces:
-        #     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        #
-        # cv2.imshow('img', img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-    #     face_res.append(faces)
-    # for idx, file_name in enumerate(file_names):
-
         id = int(file_name[0:4])  # Extract the photo-id from the filename.
         nose_x = data[id][40]
         nose_y = data[id][41]
+        face = find_face(faces, nose_x, nose_y)
 
-        chin_x = data[id][68]
-        chin_y = data[id][69]
-
-        found = False
-
-        for face in faces:
-            # import pdb; pdb.set_trace()
-            if face_contains(face, nose_x, nose_y): # and face_contains(face, chin_x, chin_y)):
-                data[id][70] = face[0]
-                data[id][71] = face[1]
-                data[id][72] = face[2]
-                data[id][73] = face[3]
-                found = True
-                print 'Update id=%d' % id
-                break;
-
-        if not found:
+        if face is None:
             print 'Could not find a matching face for %s' % file_name
             print faces
-            print nose_x, nose_y, chin_x, chin_y
+            print nose_x, nose_y
+        else:
+            (x,y,w,h) = data[id, 70:] = face
 
+            # cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            # cv2.imshow('img', img)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
 
-    np.savetxt('faces.csv', data, fmt="%0.3f")
+    # Write out the data file with additional face information.
+    np.savetxt('data_faces.csv', data, fmt="%0.3f")
+    print ''
+    print '--> Finished face detection and wrote data with faces to `data_faces.csv`'
 
 
 
